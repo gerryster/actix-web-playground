6a3a82285fabfc01be0edd0af343973c51fefbbc
Ryan Gerry <gerryster@gmail.com>
2022-08-07 09:49:20 -0500

    Add logging and file download.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{get, App, HttpResponse, HttpServer, Result, Responder};
+use log::info;
 
 #[get("/")]
 async fn root() -> impl Responder {
@@ -6,12 +8,15 @@ async fn root() -> impl Responder {
 }
 
 #[get("/file")]
-async fn file() -> impl Responder {
-    HttpResponse::Ok().body("placeholder for files")
+async fn file() -> Result<NamedFile> {
+    info!("{}", "about to serve the file");
+    Ok(NamedFile::open("docs/magna-carta.pdf")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
             .service(root)
